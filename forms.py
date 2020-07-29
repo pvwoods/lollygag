39baedb6e97c93f04139ff7790a3bee68b826d1b
Phil <pvwoods@users.noreply.github.com>
2020-07-29 16:27:19 -0700

    movement of models

--- forms.py
+++ forms.py
@@ -1,6 +1,38 @@
 import npyscreen
 from models import Task
 
+class TaskList(npyscreen.MultiLineAction):
+    def __init__(self, *args, **keywords):
+        super(TaskList, self).__init__(*args, **keywords)
+        self.add_handlers({
+            "^A": self.when_add_record,
+            "^D": self.when_delete_record
+        })
+
+    def display_value(self, vl):
+        return "%s, %s" % (vl[1], vl[2])
+
+    def actionHighlighted(self, act_on_this, keypress):
+        self.parent.parentApp.getForm('EDIT_TASK_FORM').value =act_on_this[0]
+        self.parent.parentApp.switchForm('EDIT_TASK_FORM')
+
+    def when_add_record(self, *args, **keywords):
+        self.parent.parentApp.getForm('EDIT_TASK_FORM').value = None
+        self.parent.parentApp.switchForm('EDIT_TASK_FORM')
+
+    def when_delete_record(self, *args, **keywords):
+        self.parent.parentApp.myDatabase.delete_record(self.values[self.cursor_line][0])
+        self.parent.update_list()
+
+class TaskListDisplay(npyscreen.FormMutt):
+    MAIN_WIDGET_CLASS = TaskList
+    def beforeEditing(self):
+        self.update_list()
+
+    def update_list(self):
+        self.wMain.values = self.parentApp.myDatabase.list_all_records()
+        self.wMain.display()
+
 class NewTaskForm(npyscreen.ActionFormV2):
 
     _DESCRIPTION_DEFAULT_TEXT = """Additional Context"""
@@ -14,9 +46,11 @@ class NewTaskForm(npyscreen.ActionFormV2):
         self.description = self.add(npyscreen.MultiLineEdit,
                value = self._DESCRIPTION_DEFAULT_TEXT, name="Description: ",
                max_height=5, rely=5)
-        self.priority = self.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["low","medium","high","critical"], scroll_exit=True)
-        self.tags = self.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Tags",
+        self.priority = self.add(npyscreen.TitleSelectOne, max_height=4, value = [0,], name="Priority: ",
+                values = Task._PRIORITIES, scroll_exit=True)
+        self.status = self.add(npyscreen.TitleSelectOne, max_height=4, value = [0,], name="Status: ",
+                values = Task._STATUS, scroll_exit=True)
+        self.tags = self.add(npyscreen.TitleMultiSelect, max_height =-2, value = [], name="Tags: ",
                 values = ["#help","#done","#needs"], scroll_exit=True)
     
     def on_ok(self):
@@ -26,12 +60,17 @@ class NewTaskForm(npyscreen.ActionFormV2):
             self.due.value, 
             descr, 
             self.readable_priority,
+            self.readable_status,
             self.readable_tags)
         t.save()
     
     @property
     def readable_priority(self):
         return self.priority.values[self.priority.value[0]]
+
+    @property
+    def readable_status(self):
+        return self.status.values[self.status.value[0]]
     
     @property
     def readable_tags(self):
--- models.py
+++ models.py
@@ -1,51 +1,87 @@
 import uuid
 import json
 import datetime
+import sqlite3
 
-class BaseObjectEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, uuid.UUID):
-            return obj.hex
-        if isinstance(obj, (datetime.date, datetime.datetime)):
-            return obj.isoformat()
-        return json.JSONEncoder.default(self, obj)
+class DB():
+
+    def __init__(self, filename="lollygag.db", models=[]):
+        self.dbfilename = filename
+        db = sqlite3.connect(self.dbfilename)
+        for model in models:
+            model.create_table_structure(db)
 
 class BaseObject():
 
-    _uuid = None
+    _record_id = None
+
+    def create_table_structure(db):
+        c = db.cursor()
+        c.execute(
+        f'CREATE TABLE IF NOT EXISTS ${type(self).__name__}\
+            ( _record_id INTEGER PRIMARY KEY \
+              )' \
+            )
+        db.commit()
+        c.close()
 
     def save(self):
         if(self._uuid is None):
             self._uuid = uuid.uuid4()
-        file_path = f'./scratchpad/{self._uuid.hex}.json'
+        file_path = f'./scratchpad/{type(self).__name__}/{self._uuid.hex}.json'
         with open(file_path, 'w') as filehandle:
             filehandle.write(self.toJson())
-    
-    def toJson(self):
-        return json.dumps({**self.__dict__, **{ "_type": type(self).__name__ }}, cls=BaseObjectEncoder, sort_keys=True, indent=4)
 
 class Task(BaseObject):
 
-    _PRIORITIES = {"low": 1, "medium": 2, "high": 3, "critical": 4}
+    _PRIORITIES = ["low", "medium", "high", "critical"]
+    _STATUS = ["Open", "In Progress", "Blocked",  "Complete"]
 
-    def __init__(self, title, due, description, priority, tags):
+    def create_table_structure(db):
+        c = db.cursor()
+        c.execute(
+        f'CREATE TABLE IF NOT EXISTS ${type(self).__name__}\
+            ( _record_id INTEGER PRIMARY KEY, \
+              title     TEXT, \
+              due   TEXT, \
+              description   TEXT, \
+              priority   TEXT, \
+              description TEXT \
+              )' \
+            )
+        db.commit()
+        c.close()
+
+    def __init__(self, title, due, description, priority, status, tags):
         self.title = title
         self.due = due
         self.description = description
-        if priority in self._PRIORITIES:
+        if priority in Task._PRIORITIES:
             self.priority = priority
         else:
-            self.priority = "low"
+            self.priority = Task._PRIORITIES[0]
+        if status in Task._STATUS:
+            self.status = status
+        else:
+            self.status = Task._STATUS[0]
         self.tags = tags
 
-    @property
-    def sort_score(self):
-        return self.int_priority()
-
-    def int_priority(self):
-        return self._PRIORITIES[self.priority]
-
 class Collection(BaseObject):
+
+    def create_table_structure(db):
+        c = db.cursor()
+        c.execute(
+        f'CREATE TABLE IF NOT EXISTS ${type(self).__name__}\
+            ( _record_id INTEGER PRIMARY KEY, \
+              title     TEXT, \
+              due   TEXT, \
+              description   TEXT, \
+              priority   TEXT, \
+              description TEXT \
+              )' \
+            )
+        db.commit()
+        c.close()
 
     def __init__(self, title):
         self.title = title
